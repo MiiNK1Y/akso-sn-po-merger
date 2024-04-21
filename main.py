26565e74287fabd982ff8503be815cf234919fff
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-04-21 11:11:14 +0200

    formatting and added more fail-safety

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import configparser
 import openpyxl
 
 class Excel_Workbook():
-
     def __init__(self, path: str) -> None:
         self.xlsx_file = path
         self.workbook = openpyxl.load_workbook(self.xlsx_file)
@@ -17,16 +16,21 @@ class Excel_Workbook():
     def create_sheet(self, sheet_name) -> None:
         self.workbook.create_sheet(sheet_name)
 
+    def get_blank_cells(self) -> int:
+        return self.blank_cells
+
     def get_all_column_headers(self) -> list:
         column_headers = []
         for header in self.sheet.iter_cols(1, self.sheet_width, values_only=True):
             column_headers.append(header[0])
         return column_headers
 
-    def map_data_pair(self, column_0: int, column_1: int) -> list:
+    def map_column_data_pair(self, column_0: int, column_1: int) -> list:
         column_data_pair = []
-        for row in self.sheet.iter_rows(1, self.sheet_height, values_only=True):
-            column_data_pair.append(str(row[column_0]) + "." + str(row[column_1]))
+        rows = self.sheet.iter_rows(1, self.sheet_height, values_only=True)
+        for row in rows:
+            combined_value = str(row[column_0]) + "." + str(row[column_1])
+            column_data_pair.append(combined_value)
         return column_data_pair
 
     def save_workbook(self, filename: str) -> None:
@@ -36,16 +40,17 @@ class Excel_Workbook():
         self.workbook.close() 
 
     def delete_columns(self, columns_delete: list) -> None:
-        column_index_comp = 0
+        column_index_compensation = 0
         for column in columns_delete:
-            column -= column_index_comp
+            column -= column_index_compensation
             self.sheet.delete_cols(column)
-            column_index_comp += 1
+            column_index_compensation += 1
 
     def match_and_insert(self, column_lane_index: int, column_insert_index: int, replacement_0: str, replacement_1: str, data: list) -> int:
         row_count = 1
         data_SNs = [i.split(".")[0] for i in data]
-        for row in self.sheet.iter_rows(1, self.sheet_height, values_only=True):
+        rows = self.sheet.iter_rows(1, self.sheet_height, values_only=True)
+        for row in rows:
             found_cell_value = row[column_lane_index]
             if found_cell_value == None:
                 self.sheet.cell(row=row_count, column=column_insert_index, value=replacement_0)
@@ -64,8 +69,34 @@ class Excel_Workbook():
                 self.sheet.cell(row=row_count, column=column_insert_index, value=replacement_1)
                 row_count += 1
 
-    def get_blank_cells(self) -> int:
-        return self.blank_cells
+def clr_stt(log_str: str) -> str:
+    green = "\033[92m"
+    cyan = "\033[36m"
+    yellow = "\033[33m"
+    red = "\033[41m"
+    esc = "\033[0m"
+    status = ""
+    status_end_char = 0
+    for index, i in enumerate(log_str):
+        if i == log_str[0] and (log_str[0] == "["):
+            status += i
+            continue
+        elif i == "]" and (index > 2 or index < 5):
+            status += i
+            status_end_char = index + 1
+            break
+        else:
+            status += i
+    rest_of_string = log_str[status_end_char:]
+    rest_of_sequence = status + esc + rest_of_string
+    if status == "[+]":
+        return str(green + rest_of_sequence)
+    elif status == "[x]":
+        return str(cyan + rest_of_sequence)
+    elif status == "[!]":
+        return str(yellow + rest_of_sequence)
+    elif status == "[!!]":
+        return str(red + rest_of_sequence)
 
 def date_is_valid(date: str) -> bool:
     days = int(date[0:2])
@@ -76,15 +107,15 @@ def date_is_valid(date: str) -> bool:
     else:
         return False
 
-def get_date_from_str(file_name: str) -> str | None:
-    char_list = ".0123456789" #instead of trying to convert every character to int
+def get_date_from_str(file_name: str) -> str:
+    valid_chars = ".0123456789" #instead of trying to convert every character to int
     date_format = "dd.mm.yyyy"
     for index, char in enumerate(file_name):
-        if char in char_list:
+        if char in valid_chars:
             date_format_lenght = len(date_format) + 1
             for x in range(date_format_lenght):
                 x_val = file_name[index + x]
-                if (x == 2 or x == 5) and (x_val != '.'):
+                if (x == 2 or x == 5) and (x_val != "."):
                     break
                 elif x == (date_format_lenght - 1):
                     date_found = file_name[index:(index + x)]
@@ -92,64 +123,68 @@ def get_date_from_str(file_name: str) -> str | None:
                         return date_found
                     else:
                         break
-                elif x_val not in char_list:
+                elif x_val not in valid_chars:
                     break
                 else:
                     continue
         else:
             continue
+    error_handler("Not able to get valid date from filename!")
 
 # THIS ONE IS BIG BRAIN UNIQUE BRO.
 def get_newest_date(d1: str, d2: str) -> str | None:
     if d1 == d2:
-        return None
-    d1_formated = d1.split('.')[::-1]
-    d2_formated = d2.split('.')[::-1]
-    print(f"[+] Formated date: {d1_formated}\n[+] Formated date: {d2_formated}")
+        error_handler("The dates in the filenames are the same, not able to decide which are the newest!")
+    d1_formated = d1.split(".")[::-1]
+    d2_formated = d2.split(".")[::-1]
+    print(clr_stt("[+] Formated date: ") + str(d1_formated))
+    print(clr_stt("[+] Formated date: ") + str(d2_formated))
     if d1_formated > d2_formated:
-        print(f"[+] Newest date found: {d1}")
+        print(clr_stt("[+] Newest date found: ") + d1)
         return d1
     else:
-       print(f"[+] Newest date found: {d2}")
-       return d2
+        print(clr_stt("[+] Newest date found: ") + d2)
+        return d2
 
 def get_excel_files() -> list:
-    files = os.listdir('.')
+    files = os.listdir(".")
     excel_files = []
     for file in files:
-        if file[-5:] == '.xlsx':
+        if file[-5:] == ".xlsx":
             excel_files.append(file)
-            print(f"[+] Found file: {file}")
+            print(clr_stt("[+] Found file: ") + file)
             if len(excel_files) > 2:
-                error_handler("More than 2 excel files found in directory.")
+                error_handler("More than two excel files found in directory.")
     if len(excel_files) < 2:
-        error_handler("Less than 2 excel files was found, 2 are needed for this operation.")
+        error_handler("Less than two excel files was found, two are needed for this operation.")
     return excel_files
 
-# TODO: implement more error handling across the project.
 def error_handler(msg) -> None:
-    print(f"\n[!!] Warning: {msg}\n[!!] Exit the application, fix the issue then retry.")
-    input("\n[x] Press [ENTER] to exit the application.")
+    print("")
+    print(clr_stt("[!!] Warning: ") + str(msg))
+    print(clr_stt("[!!] Exit the application, fix the issue then retry."))
+    print("")
+    input(clr_stt("[x] Press [ENTER] to exit the application."))
     exit()
 
 def main() -> None:
     print("\n----------logging started----------")
     # set configparser to read the config.
     config = configparser.ConfigParser()
-    config.read('config.ini')
-    default_config = config['DEFAULT']
+    config.read("config.ini")
+    default_config = config["DEFAULT"]
 
     # set all config.ini-values to their respective variables, making them usable.
-    old_sheet_path = default_config['old_sheet_path']
-    new_sheet_path = default_config['new_sheet_path']
-    final_sheet_path = default_config['final_sheet_path']
-    final_sheet_name = default_config['final_sheet_name']
-    columns_to_delete = default_config['columns_to_delete']
-    serial_column_text = default_config['serial_column_text']
-    po_column_text = default_config['po_column_text']
-    final_sheet_column_insert_po = default_config['final_sheet_column_insert_po']
-    none_to_match_replacement = default_config['none_to_match_replacement']
-    no_match_replacement = default_config['no_match_replacement']
+    old_sheet_path = default_config["old_sheet_path"]
+    new_sheet_path = default_config["new_sheet_path"]
+    final_sheet_path = default_config["final_sheet_path"]
+    final_sheet_name = default_config["final_sheet_name"]
+    columns_to_delete = default_config["columns_to_delete"]
+    serial_column_text = default_config["serial_column_text"]
+    po_column_text = default_config["po_column_text"]
+    final_sheet_column_insert_po = default_config["final_sheet_column_insert_po"]
+    none_to_match_replacement = default_config["none_to_match_replacement"]
+    no_match_replacement = default_config["no_match_replacement"]
 
     # converted to int, since configparser only keeps strings.
     final_sheet_column_insert_po = int(final_sheet_column_insert_po)
@@ -170,9 +205,9 @@ def main() -> None:
 
     # assign the full file-path based on previous variables, to other variables that combine the path and the file-name.
     new_sheet_path = new_sheet_path + new_sheet_name
-    print(f"[+] New sheet assigned to: {new_sheet_path}")
+    print(clr_stt("[+] New sheet assigned to: ") + new_sheet_path)
     old_sheet_path = old_sheet_path + old_sheet_name
-    print(f"[+] Old sheet assigned to: {old_sheet_path}")
+    print(clr_stt("[+] Old sheet assigned to: ") + old_sheet_path)
     final_sheet_path = final_sheet_path + final_sheet_name
     print("-----------logging ended-----------\n")
 
@@ -181,7 +216,7 @@ def main() -> None:
     new_sheet = Excel_Workbook(new_sheet_path)
 
     # need to convert "columns_to_delete" from string into list, for the compability.
-    columns_to_delete = columns_to_delete.split(',')
+    columns_to_delete = columns_to_delete.split(",")
     # use eval to convert every str-number to actual int.
     columns_to_delete = [eval(i) for i in columns_to_delete]
     new_sheet.delete_columns(columns_to_delete)
@@ -194,7 +229,7 @@ def main() -> None:
 
     old_sheet_serial_index = old_sheet_headers.index(serial_column_text)
     old_sheet_po_index = old_sheet_headers.index(po_column_text)
-    old_sheet_sn_po = old_sheet.map_data_pair(old_sheet_serial_index, old_sheet_po_index)
+    old_sheet_sn_po = old_sheet.map_column_data_pair(old_sheet_serial_index, old_sheet_po_index)
 
     final_sheet_headers = final_sheet.get_all_column_headers()
     final_sheet_serial_index = final_sheet_headers.index(serial_column_text)
@@ -209,17 +244,22 @@ def main() -> None:
 
     final_sheet_blank_POs = final_sheet.get_blank_cells()
     if final_sheet_blank_POs == 0:
-        print(f"[+] Serial numbers without PO: {final_sheet_blank_POs}\n[+] No need to add new POs :)")
+        print(clr_stt("[+] Serial numbers without PO: ") + str(final_sheet_blank_POs))
+        print(clr_stt("[+] No need to add new POs :)"))
     else:
-        print(f"[!] Serial numbers without PO: {final_sheet_blank_POs}\n[!] Remember to add the new POs !!\n[!] '{no_match_replacement}' is written where the PO should be.")
+        print(clr_stt("[!] Serial numbers without PO: ") + str(final_sheet_blank_POs))
+        print(clr_stt("[!] Remember to add the new POs!!"))
+        print(clr_stt(f"[!] '{no_match_replacement}' is written where the PO should be."))
 
     final_sheet.create_sheet("Available in stock")
     final_sheet.save_workbook(final_sheet_path)
     final_sheet.close_workbook()
     old_sheet.close_workbook()
 
-    print("\n[+] DONE!\n")
-    input("[x] Press [ENTER] to exit the application.")
+    print("")
+    print(clr_stt("[+] DONE!"))
+    print("")
+    input(clr_stt("[x] Press [ENTER] to exit the application."))
 
 if __name__ == '__main__':
     main()
